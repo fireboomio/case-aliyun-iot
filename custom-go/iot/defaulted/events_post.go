64517d76bfce99183e1c00ef1100b66e11da783a
xufeixiang <95065451+lovestaryouth@users.noreply.github.com>
2025-09-15 16:54:04 +0800

    feat: add postEventExtractor to extract items data to make same with other event struct.

--- custom-go/iot/defaulted/events_post.go
+++ custom-go/iot/defaulted/events_post.go
@@ -1,15 +1,48 @@
 package defaulted
 
 import (
+	"bytes"
 	"custom-go/customize"
 	"custom-go/iot/base"
 	"custom-go/pkg/types"
+	"github.com/buger/jsonparser"
 	"github.com/labstack/echo/v4"
+	"github.com/wundergraph/graphql-go-tools/pkg/lexer/literal"
 )
 
 func init() {
 	PostEvent.SetHandler(func(logger echo.Logger, client *types.InternalClient, properties *base.ApplicationProperties, output *base.EventTslOutput[PostEventOutput]) error {
 		go customize.PushEventData(PostEvent, properties, output)
 		return nil
+	}, base.TopicTrieOptions{
+		ExtractValue:     postEventExtractor,
+		CheckRepeatValue: true,
 	})
+}
+
+func postEventExtractor(data []byte) []byte {
+	rewriteBuffer := bytes.NewBuffer(literal.LBRACE)
+	_ = jsonparser.ObjectEach(data,
+		func(key []byte, value []byte, _ jsonparser.ValueType, _ int) error {
+			_value, _valueType, _, _err := jsonparser.Get(value, "value")
+			if _err != nil {
+				return _err
+			}
+			if rewriteBuffer.Len() > 1 {
+				rewriteBuffer.Write(literal.COMMA)
+			}
+			rewriteBuffer.Write(literal.QUOTE)
+			rewriteBuffer.Write(key)
+			rewriteBuffer.Write(literal.QUOTE)
+			rewriteBuffer.Write(literal.COLON)
+			if _valueType == jsonparser.String {
+				rewriteBuffer.Write(literal.QUOTE)
+				defer rewriteBuffer.Write(literal.QUOTE)
+			}
+			rewriteBuffer.Write(_value)
+			return nil
+		},
+		"items")
+	rewriteBuffer.Write(literal.RBRACE)
+	return rewriteBuffer.Bytes()
 }
